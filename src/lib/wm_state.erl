1624d8cdb5a699d56908fb85f56256358c301470
Taras Shapovalov <taras@iclouds.net>
2024-06-30 15:26:30 +0400

    Convert wm_state module from gen_fsm to gen_statem behaviour

--- src/lib/wm_state.erl
+++ src/lib/wm_state.erl
@@ -1,11 +1,10 @@
 -module(wm_state).
 
--behaviour(gen_fsm).
+-behaviour(gen_statem).
 
--export([start_link/1]).
--export([enter/1, breakdown/1, get_current/0]).
--export([init/1, handle_event/3, handle_sync_event/4, handle_info/3, code_change/4, terminate/3]).
--export([stopped/2, loading/2, maint/2, offline/2, idle/2]).
+-export([start_link/1, enter/1, breakdown/1, get_current/0]).
+-export([callback_mode/0, init/1, terminate/3, code_change/4]).
+-export([stopped/3, loading/3, maint/3, offline/3, idle/3]).
 
 -include("wm_log.hrl").
 
@@ -17,62 +16,40 @@
 
 -spec start_link([term()]) -> {ok, pid()}.
 start_link(_Args) ->
-    gen_fsm:start_link({local, ?MODULE}, ?MODULE, undefined, []).
+    gen_statem:start_link({local, ?MODULE}, ?MODULE, undefined, []).
 
 -spec enter(atom()) -> ok.
 enter(State) ->
     wm_conf:set_node_state(alloc, State, node()),
-    gen_fsm:send_event(?MODULE, {enter, State}).
+    gen_statem:cast(?MODULE, {enter, State}).
 
 -spec breakdown(atom()) -> ok.
 breakdown(State) ->
-    gen_fsm:send_event(?MODULE, {breakdown, State}).
+    gen_statem:cast(?MODULE, {breakdown, State}).
 
 %% @doc Get current node state
 -spec get_current() -> atom().
 get_current() ->
-    gen_fsm:sync_send_all_state_event(?MODULE, get_current).
+    gen_statem:call(?MODULE, get_current).
 
 %% ============================================================================
 %% Callbacks
 %% ============================================================================
 
+-spec callback_mode() -> state_functions.
 -spec init(term()) ->
               {ok, atom(), term()} |
               {ok, atom(), term(), hibernate | infinity | non_neg_integer()} |
               {stop, term()} |
               ignore.
--spec handle_event(term(), atom(), term()) ->
-                      {next_state, atom(), term()} |
-                      {next_state, atom(), term(), hibernate | infinity | non_neg_integer()} |
-                      {stop, term(), term()} |
-                      {stop, term(), term(), term()}.
--spec handle_sync_event(term(), atom(), atom(), term()) ->
-                           {next_state, atom(), term()} |
-                           {next_state, atom(), term(), hibernate | infinity | non_neg_integer()} |
-                           {reply, term(), atom(), term()} |
-                           {reply, term(), atom(), term(), hibernate | infinity | non_neg_integer()} |
-                           {stop, term(), term()} |
-                           {stop, term(), term(), term()}.
--spec handle_info(term(), atom(), term()) ->
-                     {next_state, atom(), term()} |
-                     {next_state, atom(), term(), hibernate | infinity | non_neg_integer()} |
-                     {stop, term(), term()}.
 -spec code_change(term(), atom(), term(), term()) -> {ok, term()}.
 -spec terminate(term(), atom(), term()) -> ok.
+callback_mode() ->
+    state_functions.
+
 init(_Args) ->
     ?LOG_DEBUG("Node state module has been started"),
     {ok, stopped, #mstate{}}.
-
-handle_sync_event(get_current, From, State, MState) ->
-    ?LOG_DEBUG("Asked for current state (~p) by ~10000p", [State, From]),
-    {reply, State, State, MState}.
-
-handle_event(_Event, State, MState) ->
-    {next_state, State, MState}.
-
-handle_info(_Info, StateName, MState) ->
-    {next_state, StateName, MState}.
 
 code_change(_OldVsn, StateName, MState, _Extra) ->
     {ok, StateName, MState}.
@@ -86,97 +63,107 @@ terminate(Status, StateName, _) ->
 %% ============================================================================
 
 %% @doc State meaning: services are unloaded, but maintanance is not needed
--spec stopped({atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
-stopped({enter, stopped}, MState) ->
+-spec stopped({call, pid()} | atom(), {atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
+stopped({call, From}, get_current, MState) ->
+    {keep_state, MState, [{reply, From, ?FUNCTION_NAME}]};
+stopped(cast, {enter, stopped}, MState) ->
     {next_state, stopped, MState};
-stopped({enter, loading}, MState) ->
+stopped(cast, {enter, loading}, MState) ->
     wm_event:announce(new_node_state, {node(), loading}),
     {next_state, loading, MState};
-stopped({enter, maint}, MState) ->
+stopped(cast, {enter, maint}, MState) ->
     wm_event:announce(new_node_state, {node(), maint}),
     {next_state, maint, MState};
-stopped({enter, breakdown}, MState) ->
+stopped(cast, {enter, breakdown}, MState) ->
     wm_event:announce(new_node_state, {node(), stopped}),
     {next_state, stopped, MState};
-stopped({enter, offline}, MState) ->
+stopped(cast, {enter, offline}, MState) ->
     wm_event:announce(new_node_state, {node(), offline}),
     {next_state, offline, MState}.
 
 %% @doc State meaning: services are loading now according the node roles
--spec loading({atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
-loading({enter, idle}, MState) ->
+-spec loading({call, pid()} | atom(), {atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
+loading({call, From}, get_current, MState) ->
+    {keep_state, MState, [{reply, From, ?FUNCTION_NAME}]};
+loading(cast, {enter, idle}, MState) ->
     {next_state, idle, MState};
-loading({enter, loading}, MState) ->
+loading(cast, {enter, loading}, MState) ->
     {next_state, loading, MState};
-loading({enter, offline}, MState) ->
+loading(cast, {enter, offline}, MState) ->
     wm_event:announce(new_node_state, {node(), offline}),
     {next_state, offline, MState};
-loading({enter, maint}, MState) ->
+loading(cast, {enter, maint}, MState) ->
     wm_event:announce(new_node_state, {node(), maint}),
     {next_state, maint, MState};
-loading({enter, stopped}, MState) ->
+loading(cast, {enter, stopped}, MState) ->
     wm_event:announce(new_node_state, {node(), stopped}),
     {next_state, stopped, MState};
-loading({enter, breakdown}, MState) ->
+loading(cast, {enter, breakdown}, MState) ->
     wm_event:announce(new_node_state, {node(), maint}),
     {next_state, maint, MState}.
 
 %% @doc State meaning: the node requires some intervention from outside
--spec maint({atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
-maint({enter, maint}, MState) ->
+-spec maint({call, pid()} | atom(), {atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
+maint({call, From}, get_current, MState) ->
+    {keep_state, MState, [{reply, From, ?FUNCTION_NAME}]};
+maint(cast, {enter, maint}, MState) ->
     {next_state, maint, MState};
-maint({enter, offline}, MState) ->
+maint(cast, {enter, offline}, MState) ->
     wm_event:announce(new_node_state, {node(), offline}),
     {next_state, offline, MState};
-maint({enter, idle}, MState) ->
+maint(cast, {enter, idle}, MState) ->
     wm_event:announce(new_node_state, {node(), idle}),
     {next_state, idle, MState};
-maint({enter, loading}, MState) ->
+maint(cast, {enter, loading}, MState) ->
     wm_event:announce(new_node_state, {node(), loading}),
     {next_state, loading, MState};
-maint({enter, breakdown}, MState) ->
+maint(cast, {enter, breakdown}, MState) ->
     wm_event:announce(new_node_state, {node(), stopped}),
     {next_state, stopped, MState};
-maint({enter, stopped}, MState) ->
+maint(cast, {enter, stopped}, MState) ->
     wm_event:announce(new_node_state, {node(), stopped}),
     {next_state, stopped, MState}.
 
 %% @doc State meaning: services are loaded, but the node should not be used now
--spec offline({atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
-offline({enter, offline}, MState) ->
+-spec offline({call, pid()} | atom(), {atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
+offline({call, From}, get_current, MState) ->
+    {keep_state, MState, [{reply, From, ?FUNCTION_NAME}]};
+offline(cast, {enter, offline}, MState) ->
     {next_state, offline, MState};
-offline({enter, loading}, MState) ->
+offline(cast, {enter, loading}, MState) ->
     wm_event:announce(new_node_state, {node(), loading}),
     {next_state, loading, MState};
-offline({enter, stopped}, MState) ->
+offline(cast, {enter, stopped}, MState) ->
     wm_event:announce(new_node_state, {node(), stopped}),
     {next_state, stopped, MState};
-offline({enter, idle}, MState) ->
+offline(cast, {enter, idle}, MState) ->
     wm_event:announce(new_node_state, {node(), idle}),
     {next_state, idle, MState};
-offline({enter, maint}, MState) ->
+offline(cast, {enter, maint}, MState) ->
     wm_event:announce(new_node_state, {node(), maint}),
     {next_state, maint, MState};
-offline({enter, breakdown}, MState) ->
+offline(cast, {enter, breakdown}, MState) ->
     wm_event:announce(new_node_state, {node(), maint}),
     {next_state, maint, MState}.
 
 %% @doc State meaning: node is ready to run jobs
--spec idle({atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
-idle({enter, idle}, MState) ->
+-spec idle({call, pid()} | atom(), {atom(), atom()}, #mstate{}) -> {atom(), atom(), #mstate{}}.
+idle({call, From}, get_current, MState) ->
+    {keep_state, MState, [{reply, From, ?FUNCTION_NAME}]};
+idle(cast, {enter, idle}, MState) ->
     {next_state, idle, MState};
-idle({enter, loading}, MState) ->
+idle(cast, {enter, loading}, MState) ->
     wm_event:announce(new_node_state, {node(), loading}),
     {next_state, loading, MState};
-idle({enter, offline}, MState) ->
+idle(cast, {enter, offline}, MState) ->
     wm_event:announce(new_node_state, {node(), offline}),
     {next_state, offline, MState};
-idle({enter, maint}, MState) ->
+idle(cast, {enter, maint}, MState) ->
     wm_event:announce(new_node_state, {node(), maint}),
     {next_state, maint, MState};
-idle({enter, stopped}, MState) ->
+idle(cast, {enter, stopped}, MState) ->
     wm_event:announce(new_node_state, {node(), stopped}),
     {next_state, stopped, MState};
-idle({enter, breakdown}, MState) ->
+idle(cast, {enter, breakdown}, MState) ->
     wm_event:announce(new_node_state, {node(), maint}),
     {next_state, maint, MState}.
